f6df323646af9efb027c3235e505a9879c1bdb30
Han Zaw Nyein <hanzaw@192.168.1.3>
2023-11-28 03:47:39 +0630

    [add] onchange endkil calculate total kilo and force_save

--- addons/kilo_taxti/models/kilo_bookig.py
+++ addons/kilo_taxti/models/kilo_bookig.py
@@ -9,3 +9,12 @@ class KiloBooking(models.Model):
     partner_id = fields.Many2one('res.partner')
     start_kilo = fields.Float()
     end_kilo = fields.Float()
+    total_kilo = fields.Float()
+
+    vehicle_id = fields.Many2one('fleet.vehicle')
+    driver_id = fields.Many2one('res.partner', related="vehicle_id.driver_id")
+
+    @api.onchange('end_kilo')
+    def _on_change_end_kilo(self):
+        if self.end_kilo:
+            self.total_kilo = self.start_kilo + self.end_kilo
